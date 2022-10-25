054a806ddeeadb252549fbd4a6a7d64ec9822120
sucso <git@0xb1e.dev>
2022-10-25 17:01:11 +0200

    tweak: xterm-256color swallows

--- config.def.h
+++ config.def.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	/* class              instance    title            tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "firefox",          NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
 	{ "St",               NULL,       NULL,            0,         0,          1,           0,        -1 },
+	{ "xterm-256color",   NULL,       NULL,            0,         0,          1,           0,        -1 },
 	{ "xst-256color",     NULL,       NULL,            0,         0,          1,           0,        -1 },
 	{ NULL,               NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
 };

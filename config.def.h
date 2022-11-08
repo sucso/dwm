bbe94faa3874fba256470a0256fa4e07557f38bc
sucso <git@0xb1e.dev>
2022-11-08 19:27:21 +0100

    tweak: s/xst/st in rules

--- config.def.h
+++ config.def.h
@@ -31,7 +31,7 @@ static const Rule rules[] = {
 	{ "firefox",          NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
 	{ "St",               NULL,       NULL,            0,         0,          1,           0,        -1 },
 	{ "xterm-256color",   NULL,       NULL,            0,         0,          1,           0,        -1 },
-	{ "xst-256color",     NULL,       NULL,            0,         0,          1,           0,        -1 },
+	{ "st-256color",      NULL,       NULL,            0,         0,          1,           0,        -1 },
 	{ NULL,               NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
 };
 
